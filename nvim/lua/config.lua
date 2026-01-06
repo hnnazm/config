397a18eed5ce100a83c5b075bba7637befe587ff
hnnazm <git@a3s.simplelogin.com>
2026-01-07 00:30:31 +0800

    setup java environment

--- nvim/lua/config.lua
+++ nvim/lua/config.lua
@@ -47,7 +47,7 @@ require("lazy").setup({
     lazy = false,
     build = ":TSUpdate",
     config = function()
-      local ts = require('nvim-treesitter')
+      local ts = require("nvim-treesitter")
       ts.setup({})
       ts.install({
         "go",
@@ -65,6 +65,7 @@ require("lazy").setup({
         "dockerfile",
         "html",
         "python",
+        "java",
         "yaml",
       })
     end
@@ -190,6 +191,64 @@ require("lazy").setup({
           { name = "path" },
         })
       })
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      dap.adapters.delve = function(callback, config)
+        if config.mode == 'remote' and config.request == 'attach' then
+          callback({
+            type = 'server',
+            host = config.host or '127.0.0.1',
+            port = config.port or '38697'
+          })
+        else
+          callback({
+            type = 'server',
+            port = '${port}',
+            executable = {
+              command = 'dlv',
+              args = { 'dap', '-l', '127.0.0.1:${port}', '--log', '--log-output=dap' },
+              detached = vim.fn.has("win32") == 0,
+            }
+          })
+        end
+      end
+
+
+      -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+      dap.configurations.go = {
+        {
+          type = "delve",
+          name = "Debug",
+          request = "launch",
+          program = "${file}"
+        },
+        {
+          type = "delve",
+          name = "Debug test", -- configuration for debugging test files
+          request = "launch",
+          mode = "test",
+          program = "${file}"
+        },
+        -- works with go.mod packages and sub packages
+        {
+          type = "delve",
+          name = "Debug test (go.mod)",
+          request = "launch",
+          mode = "test",
+          program = "./${relativeFileDirname}"
+        }
+      }
+    end
+  },
+  {
+    "nvim-java/nvim-java",
+    config = function()
+      require('java').setup()
+      vim.lsp.enable('jdtls')
     end,
   },
   -- {
